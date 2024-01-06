bf059fd3f6c460c2e8cd296cd6da86fddb986265
miosisoniii <miosisoniii@gmail.com>
2024-01-05 20:53:38 -0700

    update link to be able to display in deployed app

--- modules/resort_link_module.R
+++ modules/resort_link_module.R
@@ -9,32 +9,35 @@ resortLinkServer <- function(id, filtered_resorts, table_ready) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
-    # Dynamically render the dropdown and button based on table_ready
+    # Dynamically render the dropdown and link based on table_ready
     output$resortLinkUI <- renderUI({
       if (table_ready()) {
         tagList(
           selectInput(ns("resortDropdown"), "Select a Resort:", choices = filtered_resorts()$NAME_RESORT),
-          actionButton(ns("openMap"), "Open in Google Maps")
+          actionButton(ns("openMap"), "Open in Google Maps"),
+          uiOutput(ns("linkOutput"))  # Output for the hyperlink
         )
       }
     })
     
-    # Observe selection changes and update the dropdown choices
+    # Observe changes in the filtered list and update the dropdown choices
     observe({
       if (table_ready()) {
         updateSelectInput(session, "resortDropdown", choices = filtered_resorts()$NAME_RESORT)
       }
     })
     
-    # Open the Google Maps link when the button is clicked
+    # Observe button click to render hyperlink
     observeEvent(input$openMap, {
       selected_resort <- input$resortDropdown
       if (!is.null(selected_resort)) {
-        resort_link <- filtered_resorts() %>% 
-          filter(NAME_RESORT == selected_resort) %>% 
+        resort_link <- filtered_resorts() %>%
+          filter(NAME_RESORT == selected_resort) %>%
           pull(GMAP_LINK_RESORT)
         if (!is.null(resort_link)) {
-          utils::browseURL(resort_link)
+          output$linkOutput <- renderUI({
+            tags$a(href = resort_link, "Open in Google Maps", target = "_blank")
+          })
         }
       }
     })
